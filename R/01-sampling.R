30d3095070409702dbc0bd03c4d2ddd0aa480355
asri2 <asri.surya@outlook.com>
2020-05-22 09:35:23 +0700

    Create Subgroup based on time period.

--- R/01-sampling.R
+++ R/01-sampling.R
@@ -28,6 +28,7 @@ df <- raw_data %>%
   select("date","px.close") %>% 
   mutate(px.return=100*log(px.close/lag(px.close)))%>% #Calculate return (close-to-close)
   mutate(day=wday(date, label = TRUE)) %>% #Generate name of the day (label)
+  mutate(nday=wday(date)) %>%
   mutate(tradingday=case_when(px.close %in% NA ~0, TRUE ~1) ) #Dummy trading and nontrading days
   
 # Generate dummy variable weekday
@@ -36,7 +37,15 @@ df <- model.matrix(~0 + day, df) %>%
   bind_cols(df) %>% 
   # Drop weekend and base category (Wednesday)  
     select(day,everything(),-c(daySun,daySat))
- 
+
+# Create Subgroups based on Time Period
+df <- df %>% 
+  mutate(cg = case_when(date >= 2000 & date <2005 ~ 1,
+                        date >= 2005 & date <2010 ~ 2,
+                        date >= 2010 & date <2015 ~ 3,
+                        date >= 2015 & date <2020 ~ 4))
+
+
 df <- na.omit(df) #remove NA values
 
 # save R dataset Object
