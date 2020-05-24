898c45d572878225771dfc6634bb29a8873dc1b3
asri2 <asri.surya@outlook.com>
2020-05-24 11:35:35 +0700

    reformat tables on xttable.

--- R/02-sumtab.R
+++ R/02-sumtab.R
@@ -28,48 +28,29 @@ df <- df %>%
 period_list <- c( "2000-2004","2005-2009","2010-2014","2015-2019")
 day_names = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
 
-
-df_1 <- df %>% 
-    filter(sub_period== 1)
-df_2 <- df %>% 
-    filter(sub_period== 2)
-df_3 <- df %>% 
-    filter(sub_period== 3)
-df_4 <- df %>% 
-    filter(sub_period== 4)
-
-
 our_summary1 <-
   list("2000-2019 (Full Sample)" =
          list("Mean_sd" = ~ mean(.data$px.return, digits = getOption("qwraps2_frmt_digits", 2),),
               "Standard Deviation" = ~ sd(.data$px.return),
               "Skewness" = ~ skewness(.data$px.return),
-              "Observations" = ~ length(.data$px.return)),
-  "2000-2004" =
-         list("Mean" = ~ mean(df_1$px.return),
-              "Standard Deviation" = ~ sd(df_1$px.return),
-              "t-statistic" = ~ max(df_1$px.return),
-              "Observations" = ~ length(df_1$px.return)),
-  "2005-2009" =
-         list("Mean" = ~ mean(df_2$px.return),
-              "Standard Deviation" = ~ sd(df_2$px.return),
-              "t-statistic" = ~ max(df_2$px.return),
-              "Observations" = ~ length(df_2$px.return)),
-  "2010-2014" =
-         list("Mean" = ~ mean(df_3$px.return),
-              "Standard Deviation" = ~ sd(df_3$px.return),
-              "t-statistic" = ~ max(df_3$px.return),
-              "Observations" = ~ length(df_3$px.return)),
-  "2014-2019" =
-         list("Mean" = ~ mean(df_4$px.return),
-              "Standard Deviation" = ~ sd(df_4$px.return),
-              "t-statistic" = ~ max(df_4$px.return),
-              "Observations" = ~ length(df_4$px.return)))
+              "Observations" = ~ length(.data$px.return)))
 
 ### Overall
 whole <- summary_table(dplyr::group_by(df, day), our_summary1)
+wholex <- xtable(whole, digits = c(2,2,2,0,0,2))
+align(wholex) <- "lrrrr" #alignment
+addtorow <- list()
+addtorow$pos <- list(0,0)
 
-print(whole,
-      cnames = day_names)
-#+end~
-print(xtable(whole, type = "latex"), file = "filename2.tex")
+#header
+addtorow$command <- c("& Monday & Tuesday & Wednesday & Thursday & Friday\\\\\n",
+                      "& Mean& St.dev & T-stat & Observations\\\\\n")
+
+names(wholex) <- c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+#print(xtable(whole, type = "latex", caption = "Summary Statistics of Return", digits = c(0,0,0,2,2,1)),
+#      file = "filename2.tex", caption.placement = "top")
+
+print(wholex, caption = "Summary Statistics of Return", include.colnames = TRUE, type = "latex", 
+      floating = FALSE,
+      include.rownames=TRUE, file="ini.tex", caption.placement = "top")
+
