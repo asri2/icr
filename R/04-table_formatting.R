f639208054fa56ab39f0a3ea4ad64ac0fd133de8
asrisurya <asri.surya@outlook.com>
2020-05-30 21:37:36 +0700

    Working on setup column_name and \backlash

--- R/04-table_formatting.R
+++ R/04-table_formatting.R
@@ -8,19 +8,19 @@ library(xtable)
 dfx <- readRDS(file = "data/garch_clean.rds") #load dataset
 #convert to matrix
 df <-as.matrix(dfx)
+k <- c("Constant","", "Monday","", "Tuesday","", "Thursday","", "Friday","", "$\\alpha_1$","","$\\gamma_1$","", "Monday","", "Tuesday","", "Thursday","", "Friday","","$\\alpha_0$","")
+df <- cbind(k,df)
+df <- xtable(df, digits=5)
+df <- as.data.frame(df)
 
-day_name <- list("Constant","", "Monday","", "Tuesday","", "Thursday","", "Friday")
-
+colnames(df) <- c("$alpha_0$", "2000-2004" , "2005-2009" , "2010-2014" , "2015-2019" ) #column names
 # 5 columns; 24 rows
 
-df <- xtable(df, digits=6)
-align(df) <- "lrrrr"
-colnames(df) <- c( "2000-2004" , "2005-2009" , "2010-2014" , "2015-2019" )
-k <- list("Constant","", "Monday","", "Tuesday","", "Thursday","", "Friday","", "$\alpha_1$","_","$\gamma_1$","", "Monday","", "Tuesday","", "Thursday","", "Friday","","$\alpha_0","")
-# 5 columns; 24 rows
-
-print(xtable(df, type = "latex"), align="rrrr",include.colnames = TRUE, type = "latex", floating = TRUE, include.rownames = FALSE, file = "output/tab_01.tex")
+print(xtable(df, type = "latex"), align="lrrrr",include.colnames = TRUE, type = "latex", floating = TRUE, floating.environment = " sidewaystable", include.rownames = FALSE, file = "output/tab_01.tex")
 
 fileConn<-file("output/table_main.rnw")
-writeLines(c("\\documentclass{article}","\\begin{document}",paste0("\\input{tab_01.tex}"),"\\end{document}"), fileConn)
+writeLines(c("\\documentclass{article}","\\usepackage{rotating}","\\begin{document}","Table 1. Modified GARCH Estimation","$\\echo_1$",paste0("\\input{tab_01.tex}"),"\\end{document}"), fileConn)
 close(fileConn)
+
+# Export PDF
+#R CMD Sweave --pdf my_sweave_file.Rnw
